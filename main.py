261e3a30dfb5aa8c52c235c1ff6a2c12d8d3cfdb
totoluto <94611749+totoluto@users.noreply.github.com>
2022-07-13 16:21:14 +0200

    added create playlist function

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from cgi import test
 from pprint import pprint
+from random import seed
+import this
 import urllib
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -13,6 +15,7 @@ SPOTIPY_CLIENT_ID = credentials.SPOTIPY_CLIENT_ID
 SPOTIPY_CLIENT_SECRET = credentials.SPOTIPY_CLIENT_SECRET
 SPOTIPY_REDIRECT_URI = credentials.SPOTIPY_REDIRECT_URI
 SCOPE = credentials.SCOPE
+PLAYLIST = credentials.PLAYLIST
 
 # Spotify definition of authentication
 spfy = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID,
@@ -52,12 +55,29 @@ def searchHtml():
     with open("songs.json", "w") as outfile:
         outfile.write(json_object)
 
+def searchAndCreateSpotify():
+    songname = []
+    spotifyLink = []
+    f = open("songs.json", encoding="utf8")
+    json_object = json.load(f)
+    
+    for object in json_object["track"]:
+        songname.append(object["name"])
+    for song in songname:
+        fylink = (spfy.search(song, limit=1))
+        try:
+            spotifyLink.append(fylink["tracks"] ["items"] [0] ["uri"])
+        except:
+            print("Song '" + song + "' wurde nicht gefunden.")
+    spfy.playlist_add_items(PLAYLIST, spotifyLink)
+
 # Main
 
 if internet_on():
     url = input("Enter URL of Apple Playlist: \n")
     fetchApplePlaylist(url=url)
     searchHtml()
+    searchAndCreateSpotify()
 else:
     print("No Connection")
     exit
